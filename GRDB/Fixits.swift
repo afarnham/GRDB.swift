9c38f8ba1a39f667a7d22d895476f997a3333e26
Gwendal Roué <gr@pierlis.com>
2021-12-19 16:00:22 +0100

    BREAKING: throwing DatabaseWriter.barrierWriteWithoutTransaction
    It used to be rethrowing, but it was then unable to deal with closed databases.
    
    The async version is modified accordingly: it accepts a Result<Database, Error>, instead of a raw Database.
    
    DatabaseMigrator.asyncMigrate is impacted as well: it accepts a Result<Database, Error>, instead of a (Database, Error?) tuple.

--- GRDB/Fixits.swift
+++ GRDB/Fixits.swift
@@ -1,1 +1,9 @@
 // Fixits for changes introduced by GRDB 6.0.0
+
+extension DatabaseMigrator {
+    @available(*, unavailable, message: "The completion function now accepts one Result<Database, Error> argument")
+    public func asyncMigrate(
+        _ writer: DatabaseWriter,
+        completion: @escaping (Database, Error?) -> Void)
+    { preconditionFailure() }
+}
